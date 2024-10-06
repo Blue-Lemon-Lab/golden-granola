d7e0443e33408d16424ce778fcc52478796e145e
jjanmo <jjanmo07@hanmail.net>
2024-10-06 11:05:41 +0900

    feat: 숫자야구 체크로직 구현

--- jjanmo/src/main/kotlin/baseball/Main.kt
+++ jjanmo/src/main/kotlin/baseball/Main.kt
@@ -1,18 +1,47 @@
 package baseball
 
+data class GameResult(val strike: Int, val ball: Int, val out: Int)
 
 class Baseball(private val total: Int = 3) {
     val generatedNumbers: List<Int> = (0..9).toList().shuffled().slice(0 until total)
+    private var inning: Int = 0
+
+    fun checkNumbers(inputNumbers: List<Int>): GameResult {
+        var strikes = 0
+        var ball = 0
+        var out = 0
+        for (i in inputNumbers.indices) {
+            when {
+                inputNumbers[i] == generatedNumbers[i] -> strikes++
+                inputNumbers[i] != generatedNumbers[i] && generatedNumbers.contains(inputNumbers[i]) -> ball++
+            }
+        }
+        if (strikes == 0 && ball == total) out = 1
+        inning++
+        return GameResult(strikes, ball, out)
+    }
+
+    fun printGameResult(result: GameResult) {
+        println("$inning 이닝")
+        when {
+            result.ball == 0 -> println("Strike $result.strike Ball $result.ball")
+            result.ball != 0 -> println("$inning Out")
+        }
+    }
 }
 
 
 fun main() {
     println("0~9 사이의 숫자를 입력하세요.")
     println("입력방법 : [숫자1 숫자2 숫자3] ← 숫자사이 공백 필수")
-    val input = readln()
-    println(input)
+    val inputNumbers = readln().split(" ").map { it.toInt() }
+
 
     val game = Baseball()
-    val generatedNumbers = game.generatedNumbers
-    println(generatedNumbers)
+    println(inputNumbers)
+    println(game.generatedNumbers)
+
+    val result = game.checkNumbers(inputNumbers)
+    game.printGameResult(result)
+
 }
