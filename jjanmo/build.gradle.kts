d474266d7806d6868afc7da030387b8d5f09bb3a
jjanmo <jjanmo07@hanmail.net>
2024-10-27 15:32:15 +0900

    feat: 코루틴 시리얼라이즈 설정

--- jjanmo/build.gradle.kts
+++ jjanmo/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.23"
+    kotlin("plugin.serialization") version "1.9.23"
 }
 
 group = "org.example"
@@ -11,7 +12,8 @@ repositories {
 
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3") // 코루틴 설정
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0") // 코루틴 시리얼라이즈 설정
 }
 
 tasks.test {
